df57caa4406305c3d95acfb7b8936d062a3168b0
NNQ148 <nnq148@gmail.com>
2025-06-03 20:46:50 +0700

    upt api

--- server.js
+++ server.js
@@ -20,8 +20,52 @@ connectDB();
 // API: Danh sách người dùng
 
 app.get("/user/list", (req, res) => {
-    
+    try {
+        const users = User.find().select("_id first_name last_name").lean();
+        return {
+            _id: users._id,
+            first_name: users.first_name, 
+            last_name: users.last_name
+        };
+    }
+    catch (err) {
+        console.error("Error fetching user list:", err);
+        res.status(500).json({ error: "Server error" });
+    }
 });
+
+// API: Chi tiết người dùng
+app.get("/user/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+      .select("_id first_name last_name location description occupation")
+      .lean();
+    if (!user) {
+      return res.status(400).json({ error: "Invalid user ID" });
+    }
+    res.json(user);
+  } catch (err) {
+    console.error("Error fetching user:", err);
+    res.status(400).json({ error: "Invalid user ID" });
+  }
+});
+
+// API: Danh sách ảnh của người dùng
+app.get("/photosOfUser/:id",async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id).lean();
+        if(!user) {
+            return res.status(400).json({ error: "Invalid user ID" });
+        }
+        const photos = await Photo.find({user_id: req.params.id}) // hoặc {user_id: user._id}
+            .select()
+    }
+    catch (err) {
+        console.error("Error fetching photos of user:", err);
+        res.status(400).json({ error: "Server error" });
+    }
+});
+
 
 // Khởi động server
 app.listen(port, () => {
